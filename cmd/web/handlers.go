654d8b0e523ce8ed56e5e8e09edc7485441507b7
Praveen005 <praveen08h@gmail.com>
2024-06-10 09:14:43 +0530

    Refactor: de-clutter snippetView() handler, delegate template parsing to render() method

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -13,8 +12,7 @@ import (
 
 func (app *application)home(w http.ResponseWriter, r* http.Request){
 	if r.URL.Path != "/"{
-		// http.NotFound(w, r)
-		app.notFound(w) //from the helpers.go file
+		app.notFound(w)
 		return
 	}
 
@@ -52,32 +50,10 @@ func (app *application) snippetView(w http.ResponseWriter, r * http.Request){
 	}
 
 	
-	// Initialize a slice containing the paths to the view.tmpl file,
-	// plus the base layout and navigation partial that we made earlier.
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/view.tmpl",
-	}
-
-	// Parse the template files
-	ts, err := template.ParseFiles(files...)
-	if err != nil{
-		app.serverError(w, err)
-		return
-	}
-	// Create an instance of a templateData struct holding the snippet data.
-	data := &templateData{
+	// Use the render helper
+	app.render(w, http.StatusOK, "view.tmpl", &templateData{
 		Snippet: snippet,
-	}
-
-
-	// And then execute them. Notice how we are passing in the snippet
-	// data (a models.Snippet struct) as the final parameter?
-	err = ts.ExecuteTemplate(w, "base", data)
-	if err != nil{
-		app.serverError(w, err)
-	}
+	})
 
 }
 
