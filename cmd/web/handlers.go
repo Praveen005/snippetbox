59760034a7e74c00a63e2b7556b752a8f7eed745
Praveen005 <praveen08h@gmail.com>
2024-06-23 01:05:28 +0530

    chore: remove redundant flash message from snippetView handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -116,14 +116,14 @@ func (app *application) snippetView(w http.ResponseWriter, r * http.Request){
 	// PopString() also deletes the key and value from the session data, so it
 	// acts like a one-time fetch. If there is no matching key in the session
 	// data this will return the empty string.
-	flash := app.sessionManager.PopString(r.Context(), "flash")
+	// flash := app.sessionManager.PopString(r.Context(), "flash")
 
 
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
 
 	// Pass the flash message to the template.
-	data.Flash = flash
+	// data.Flash = flash
 	// Use the render helper
 	app.render(w, http.StatusOK, "view.tmpl", data)
 
